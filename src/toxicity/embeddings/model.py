d3e994907fd4b1ed38c241fe55cfcca44d73cc2c
Daniel Henrique Lelis de Almeida <daniel111906@gmail.com>
2024-06-30 23:30:12 -0300

    split validation and test for re-delivery

--- src/toxicity/embeddings/model.py
+++ src/toxicity/embeddings/model.py
@@ -5,10 +5,11 @@ import torch
 import torch.nn as nn
 from torch.utils.data import Dataset
 
+
 def encode_tokens(
-    tokens: list[str], 
-    embeddings: dict[str, list[int]], 
-    emb_dim: int, 
+    tokens: list[str],
+    embeddings: dict[str, list[int]],
+    emb_dim: int,
     seq_len: int,
 ) -> np.array:
     hells_v = np.zeros((seq_len, emb_dim), dtype=np.float32)
@@ -21,19 +22,21 @@ def encode_tokens(
             hells_v[i] = embeddings[token]
         else:
             hells_v[i] = np.zeros(emb_dim)
-    
+
     return hells_v.flatten()
 
+
 class EmbeddingModel(torch.nn.Module):
-    def __init__(self, 
-                emb_dim: int,
-                seq_len: int,
+    def __init__(
+        self,
+        emb_dim: int,
+        seq_len: int,
     ):
-        super(EmbeddingModel,self).__init__()
-        self.fc1 = nn.Linear(emb_dim*seq_len, 512,dtype=torch.float32)
-        self.fc2 = nn.Linear(512,512,dtype=torch.float32)
-        self.fc3 = nn.Linear(512,256,dtype=torch.float32)
-        self.fc4 = nn.Linear(256,1,dtype=torch.float32)
+        super(EmbeddingModel, self).__init__()
+        self.fc1 = nn.Linear(emb_dim * seq_len, 512, dtype=torch.float32)
+        self.fc2 = nn.Linear(512, 512, dtype=torch.float32)
+        self.fc3 = nn.Linear(512, 256, dtype=torch.float32)
+        self.fc4 = nn.Linear(256, 1, dtype=torch.float32)
 
     def forward(self, data):
         temp = torch.relu(self.fc1(data))
@@ -43,13 +46,14 @@ class EmbeddingModel(torch.nn.Module):
 
 
 class EmbeddingDataset(Dataset):
-    def __init__(self, 
-                 df: pl.DataFrame, 
-                 token_column: str, 
-                 target_column: str, 
-                 embeddings: dict[str, list[int]], 
-                 emb_dim: int, 
-                 seq_len: int,
+    def __init__(
+        self,
+        df: pl.DataFrame,
+        token_column: str,
+        target_column: str,
+        embeddings: dict[str, list[int]],
+        emb_dim: int,
+        seq_len: int,
     ):
         self.tokens = df[token_column]
         self.target = df[target_column]
@@ -67,8 +71,12 @@ class EmbeddingDataset(Dataset):
 
         self.cache[index] = {
             "data": torch.tensor(
-                encode_tokens(self.tokens[index], embeddings=self.embeddings, 
-                              emb_dim=self.emb_dim, seq_len=self.seq_len), 
+                encode_tokens(
+                    self.tokens[index],
+                    embeddings=self.embeddings,
+                    emb_dim=self.emb_dim,
+                    seq_len=self.seq_len,
+                ),
                 dtype=torch.float32,
             ),
             "target": torch.tensor(self.target[index], dtype=torch.float32),
